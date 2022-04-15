2418bbf40a9b245311c80c92ff8c119a79e58374
Bobogok <vania.morskov@yandex.ru>
2022-04-15 21:44:09 +0300

    Отрефакторил _document и _app для MUI

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,14 +1,30 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { wrapper } from '../store';
+import createEmotionCache from '../src/createEmotionCache';
+import { CacheProvider } from '@emotion/react';
+import theme from '../src/theme';
 
-const WrappedApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+function WrappedApp(props: any) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
-    <>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </>
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
-};
+}
 
 export default wrapper.withRedux(WrappedApp);
