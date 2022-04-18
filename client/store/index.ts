3140b8e7064b3628f0db88211cdf77574325707a
Bobogok <vania.morskov@yandex.ru>
2022-04-18 23:26:46 +0300

    подключил девтулзы для редакса

--- client/store/index.ts
+++ client/store/index.ts
@@ -1,8 +1,22 @@
 import { reducer, RootState } from './reducers';
-import { createStore, Store } from 'redux';
-import { createWrapper, Context } from 'next-redux-wrapper';
+import { applyMiddleware, compose, createStore, Store } from 'redux';
+import { createWrapper } from 'next-redux-wrapper';
 
-const makeStore = (context: Context) => createStore(reducer);
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+let composeEnhancers = compose;
+if (typeof window !== 'undefined') {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
+const makeStore = (context: any) => {
+  const store = createStore(reducer, composeEnhancers());
+  return store;
+};
 
 export const wrapper = createWrapper<Store<RootState>>(makeStore, {
   debug: true,
