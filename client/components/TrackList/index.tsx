91b4a502a0b8769a9bdec3063444ffcfa2a8ec7b
Bobogok <vania.morskov@yandex.ru>
2022-05-03 23:52:01 +0300

    Оптимзировал перерендер треков

--- client/components/TrackList/index.tsx
+++ client/components/TrackList/index.tsx
@@ -1,10 +1,16 @@
 import { Box, Grid } from '@mui/material';
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import TrackItem from '../TrackItem';
 import { TrackListProps } from './props/TrackListProps';
 
 const TrackList: React.FC<TrackListProps> = ({ tracks }) => {
+  useTypedSelector(
+    (state) => state.player,
+    (curr, next) => {
+      return curr.pause === next.pause && curr.active?._id === next.active?._id;
+    },
+  );
   return (
     <Grid container direction={'column'}>
       <Box p={2}>
