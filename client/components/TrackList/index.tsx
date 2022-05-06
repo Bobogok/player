02f9b202fa1aff1ea21c7b84923abc3208e37f79
Bobogok <vania.morskov@yandex.ru>
2022-05-06 23:37:19 +0300

    Изменена главная страница

--- client/components/TrackList/index.tsx
+++ client/components/TrackList/index.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import { PlayerState } from '../../types/player';
 import TrackItem from '../TrackItem';
 import { TrackListProps } from './props/TrackListProps';
 import { SChart, STracks, STrack, SContainer } from './style';
+
+export const SSidebar = styled.div`
+  position: relative;
+  flex: 1 1 100%;
+  max-width: 40%;
+  padding-left: 30px;
+`;
+
+export const STeaserWrapper = styled.div`
+  position: sticky;
+  top: 90px;
+  width: 100%;
+  // padding-bottom: 15px;
+`;
+
+export const STeaser = styled.a`
+  // padding: 15px 0;
+  display: flex;
+  justify-content: center;
+  // width: 300px;
+  height: 100px;
+  background-color: ${({ theme }) => theme.main};
+  margin-bottom: 20px;
+  align-items: center;
+  border-radius: 5px;
+`;
+
+export const SText = styled.b`
+  font-weight: 700;
+  font-size: 30px;
+`;
+
+const teasers = [
+  {
+    text: 'Было популярно',
+  },
+  {
+    text: 'Слушают прямо сейчас',
+  },
+  {
+    text: 'Бесконечный поток',
+  },
+];
 
 const TrackList: React.FC<TrackListProps> = ({ tracks }) => {
   const isEqual = (curr: PlayerState, next: PlayerState) => {
@@ -24,6 +68,15 @@ const TrackList: React.FC<TrackListProps> = ({ tracks }) => {
           ))}
         </STracks>
       </SContainer>
+      <SSidebar>
+        <STeaserWrapper>
+          {teasers.map((teaser) => (
+            <STeaser>
+              <SText>{teaser.text}</SText>
+            </STeaser>
+          ))}
+        </STeaserWrapper>
+      </SSidebar>
     </SChart>
   );
 };
--- client/components/TrackList/style/index.ts
+++ client/components/TrackList/style/index.ts
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 export const SChart = styled.main`
   margin: 30px auto;
   max-width: 1366px;
+  display: flex;
 `;
 
 export const SContainer = styled.div`
   position: relative;
+  width: 60%;
 `;
 
 export const STracks = styled.ul`
